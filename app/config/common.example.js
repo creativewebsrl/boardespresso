6565d314632e35d3441d3ea70b6e918ac5de65a6
Riccardo Attilio Galli <riccardo@sideralis.org>
2012-05-09 16:44:43 +0200

    Simplified config files management

---
+++ app/config/common.example.js
@@ -0,0 +1,9 @@
+
+/* Copy this file to common.js and put there your sensitive data */
+
+module.exports = {
+  'session_secret': 'super secret here',
+  'server' : {
+    'port' : 3000
+  }
+};
---
+++ app/config/database.example.js
@@ -0,0 +1,8 @@
+
+/* Copy this file to database.js and put there your sensitive data */
+
+module.exports = {
+    host : 'localhost',
+    port : '1234',
+    databaseName : 'dbName'
+}
